10dce22dc45571f48db0acf01c9160cb9591e228
Pirate0101 <prodipto.dhar@tier5.in>
2024-12-24 03:31:25 +0530

    Palindrom

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ var reverseString = (StringValue) => {
   return StringValue.split("").reverse().join("");
 };
 console.log(reverseString("Reverse String"));
+
+// 2. Check for Palindrome
+// Write a function isPalindrome that checks if a given string is a palindrome.
+
+var isPalindrom = (StringValue) => {
+  StringValue === StringValue.split("").reverse().join("")
+    ? console.log(StringValue + " is a Palindrom")
+    : console.log(StringValue + " is not a Palindrom");
+};
+console.log(isPalindrom("ABCDIDCBA"));
